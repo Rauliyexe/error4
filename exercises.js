676787c2f7dc3145808d25484fd356afb1a51131
Rauliyexe <rauliyfreitas10@gmail.com>
2024-07-23 00:12:08 -0300

    Add files via upload

---
+++ exercises.js
@@ -0,0 +1,33 @@
+const exercises = [
+    {
+      portuguese: "Eu gosto de correr",
+      spanish: "Me gusta correr",
+      list: ["me", "comes", "correr", "gusta", "como", "naranja"],
+    },
+    {
+      portuguese: "O macarrão está frio",
+      japanese: "パスタは冷たい",
+      list: ["そば", "寒い", "冷たい", "パスタ", "暑い", "は"],
+    },
+    {
+      portuguese: "Meu tio é legal",
+      english: "My uncle is cool",
+      list: ["grandma", "rocks", "uncle", "my", "is", "cool"],
+    },
+    {
+        portuguese: "O céu é bonito",
+        nheengatu: "Iwaka purangasawaté",
+        list: ["puranga", "pirayarawa", "suikíri", "amana", "iwaka", "masuisáwa"],
+      },
+    {
+      portuguese: "Ontem ele caiu",
+      german: "Gestern fiel er",
+      list: ["Heute", "gestern", "schläft", "er", "fiel", "Sie"],
+    },
+    {
+        portuguese: "Ela canta bem",
+        french: "Elle chante bien",
+        list: ["elle", "bien", "balle", "chante", "crie", "il"],
+      },
+    
+  ];
